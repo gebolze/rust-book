c31746072de6a5a8449412ae3846d7d294bdd094
Markus Burkert <gebolze@gmail.com>
2019-05-27 16:41:51 +0200

    Chapter 3 - Added integer sample

---
+++ chapter3/data-types/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // integer types
+    let a: i32 = 8i32;
+    
+    println!("The value of a is: {}", a);
+
+    // supported integer types:
+    // i8   u8   - 8-bit signed / unsigned ints
+    // i16  u16  - 16-bit signed / unsigned ints
+    // i32  u32  - 32-bit signed / unsigned ints
+    // i64  u64  - 64-bit signed / unsigned ints
+    // i128 u128 - 128-bit signed / unsigned ints
+
+    // isize usize - arch dependant (usally used for indices)
+}
