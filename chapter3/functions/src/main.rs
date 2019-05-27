a11efc363b8e3e80cc0532573c53eae72a51cdde
Markus Burkert <gebolze@gmail.com>
2019-05-27 19:09:00 +0200

    Chapter 3 - Added return value sample

--- chapter3/functions/src/main.rs
+++ chapter3/functions/src/main.rs
@@ -14,9 +14,24 @@ fn main() {
     };
 
     println!("The value of y is: {}", y);
+
+    // calling functions that return a value
+    let x = five();
+    println!("The value of x is: {}", x);
+
+    let x = plus_one(5);
+    println!("plus_one(5) = {}", x);
 }
 
 fn another_function(x: i32, y: i32) {
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
