fd4bc770fd220e0957c5b509c824c59affa8e8c0
Markus Burkert <gebolze@gmail.com>
2019-05-27 14:41:38 +0200

    Chapter 3 - Added constants sample

--- chapter3/variables/src/main.rs
+++ chapter3/variables/src/main.rs
@@ -1,5 +1,7 @@
+const MAX_POINTS: u32 = 100_000;
+
 fn main() {
-    let mut x = 5;
+    let mut x = MAX_POINTS;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
