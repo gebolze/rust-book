d492d4a0532c02b50b67775fbac804dc5c087e0d
Markus Burkert <gebolze@gmail.com>
2019-05-27 16:45:08 +0200

    Chapter 3 - Added floating-point sample

--- chapter3/data-types/src/main.rs
+++ chapter3/data-types/src/main.rs
@@ -10,6 +10,19 @@ fn main() {
     // i32  u32  - 32-bit signed / unsigned ints
     // i64  u64  - 64-bit signed / unsigned ints
     // i128 u128 - 128-bit signed / unsigned ints
-
+    // default type for integers is i32
     // isize usize - arch dependant (usally used for indices)
+
+
+
+    // floating-point types
+
+    let b: f32 = 3.14;
+
+    // supported floating-point types
+    // f32 - 32-bit IEEE-754 floating-point number
+    // f64 - 64-bit IEEE-754 floating-point number
+    // default type for floats is f64 
+
+    println!("The value of b is: {}", b);
 }
