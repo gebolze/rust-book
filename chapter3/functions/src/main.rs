a96fb77a2b59cb72c8e48ff25b1d485168d4f3bc
Markus Burkert <gebolze@gmail.com>
2019-05-27 19:04:18 +0200

    Chapter 3 - Added statement and expression sample

--- chapter3/functions/src/main.rs
+++ chapter3/functions/src/main.rs
@@ -2,6 +2,18 @@ fn main() {
     println!("Hello, world!");
 
     another_function(5, 6);
+
+    // definitions
+    // statements := instructions that perform some action and
+    //               *not* return a value
+    // expressions := evaluate to a resulting value
+
+    let y = {
+        let x = 3;  // statement
+        x + 1       // expression
+    };
+
+    println!("The value of y is: {}", y);
 }
 
 fn another_function(x: i32, y: i32) {
