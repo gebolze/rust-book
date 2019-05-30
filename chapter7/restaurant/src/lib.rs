f2ad5b8f17d61d7d5f55c230fcc437753d909df5
Markus Burkert <gebolze@gmail.com>
2019-05-30 15:50:48 +0200

    Chapter 7 - Added public enum sample

--- chapter7/restaurant/src/lib.rs
+++ chapter7/restaurant/src/lib.rs
@@ -31,6 +31,11 @@ mod back_of_house {
         }
     }
 
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
     fn fix_incorrect_order() {
         cook_order();
         super::serve_order();
@@ -50,4 +55,7 @@ pub fn eat_at_restaurant() {
     // The next line won't compile if we uncomment it; we're not allowed
     // to see or modify the seasonal fruit that comes with the meal
     // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
