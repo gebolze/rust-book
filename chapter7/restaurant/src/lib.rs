806d2807bf72da53734d63531342d17786754b62
Markus Burkert <gebolze@gmail.com>
2019-05-30 18:59:51 +0200

    Chapter 7 - Add pub use sample

--- chapter7/restaurant/src/lib.rs
+++ chapter7/restaurant/src/lib.rs
@@ -44,7 +44,13 @@ mod back_of_house {
     fn cook_order() {}
 }
 
+pub use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    
     // Order a breakfast in the summer with Rye toast
     let mut meal = back_of_house::Breakfast::summer("Rye");
 
