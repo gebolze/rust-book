37bfa0650a3642fd1a015c6aa09da5e651af1188
Markus Burkert <gebolze@gmail.com>
2019-06-02 19:18:59 +0200

    Chapter 9 - encapsulated validation in custom type

--- chapter9/custom_validation/src/main.rs
+++ chapter9/custom_validation/src/main.rs
@@ -1,6 +1,24 @@
 use std::io;
 use std::cmp::Ordering;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
 
     let secret_number = 57;
@@ -16,12 +34,9 @@ fn main() {
             Err(_) => continue,
         };
 
-        if (guess < 1 || guess > 100) {
-            println!("The secret number will be between 1 and 100.0");
-            continue;
-        }
+        let guess = Guess::new(guess);
 
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Greater => println!("Your guess is too high"),
             Ordering::Less => println!("Your guess it to low"),
             Ordering::Equal => break,
