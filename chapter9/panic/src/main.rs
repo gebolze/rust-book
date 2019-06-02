5c2c170d6c6195897f9bb9cb143c55d4b880572c
Markus Burkert <gebolze@gmail.com>
2019-06-02 12:29:04 +0200

    Chapter 9 - Updated panic to abort in release mode

--- chapter9/panic/src/main.rs
+++ chapter9/panic/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
+    // by default panic! does unwind the stack, as this can be a lot of work.
+    // To prevent stack unwinding, you could modify the cargo.toml to abort
+    // instead, which won't cleanup the memory. (see cargo.toml)
     panic!("crash and burn");
 }
