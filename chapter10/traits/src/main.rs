d2c5727904bf507da92522fb2066bb87b7b2687d
Markus Burkert <gebolze@gmail.com>
2019-06-03 21:12:56 +0200

    Chapter 10 - Added usage code for the trait

--- chapter10/traits/src/main.rs
+++ chapter10/traits/src/main.rs
@@ -29,4 +29,12 @@ impl Summary for Tweet {
 }
 
 fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 tweet: {}", tweet.summarize());
 }
