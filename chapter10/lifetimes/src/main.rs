b9bb75845f9ae2eed2e0388d102c30001fc5922a
Markus Burkert <gebolze@gmail.com>
2019-06-05 07:48:15 +0200

    Chapter 10 - Added lifetime annotations to sample

--- chapter10/lifetimes/src/main.rs
+++ chapter10/lifetimes/src/main.rs
@@ -4,10 +4,24 @@ fn main() {
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
+
+
+    // The following code won't compile because string3 doesn't live long
+    // enough. Even if we as humans are able to determine that there won't
+    // be a lifetime issue, because longest will return a reference to string1,
+    // which lives long enough to be printed out.
+
+    // let result;
+    // {
+    //     let string3 = String::from("a");
+    //     result = longest(string1.as_str(), string3.as_str());
+    // }
+
+    // println!("The longest string is {}", result);
 }
 
 // The following function won't compile. 
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
