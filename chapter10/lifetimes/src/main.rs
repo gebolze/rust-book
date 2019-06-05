50ec1f8129c5d3e99c5fbc6b37cfed08bf065aef
Markus Burkert <gebolze@gmail.com>
2019-06-05 08:07:08 +0200

    Chapter 10 - Added struct lifetime sample

--- chapter10/lifetimes/src/main.rs
+++ chapter10/lifetimes/src/main.rs
@@ -1,3 +1,8 @@
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 fn main() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
@@ -18,6 +23,15 @@ fn main() {
     // }
 
     // println!("The longest string is {}", result);
+
+
+    // Lifetime Annotations and Struct Definitions
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+    let i = ImportantExcerpt { part: first_sentence };
+    println!("Important excerpt: {:?}", i);
 }
 
 // The following function won't compile. 
